c2fdaff272a5d5d79e94a2c28fd1c5e8c1c23c63
Thiago <thiago@sygmasistemas.com.br>
2026-04-22 15:56:27 -0300

    fix: autoComplete off na senha para suprimir prompt do iOS

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="gado-v13";
+const CACHE="gado-v14";
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["/","/index.html"])));
   self.skipWaiting();
